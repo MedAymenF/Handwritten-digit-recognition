9270fb3d682e1de6088680fce18d8ce4a68b9892
Mohamed Aymane Farhi <mfarhi@student.1337.ma>
2023-07-10 15:58:35 +0100

    Add data normalization and remove MSE calculation.

--- nn.py
+++ nn.py
@@ -8,15 +8,18 @@ from model import MultiLayerPerceptron
 def check_nn_gradients():
     input_layer_size = 3
     hidden_layer_1_size = 4
-    hidden_layer_2_size = 4
+    hidden_layer_2_size = 6
     num_labels = 3
     m = 5
     lambda_ = 2
 
-    architecture = [input_layer_size, hidden_layer_1_size,
-                    hidden_layer_2_size, num_labels]
-    mlp = MultiLayerPerceptron(architecture,
-                               lambda_=lambda_, output_layer='softmax')
+    architecture = [
+        [input_layer_size, None],
+        [hidden_layer_1_size, 'sigmoid'],
+        [hidden_layer_2_size, 'relu'],
+        [num_labels, 'softmax']
+    ]
+    mlp = MultiLayerPerceptron(architecture, lambda_=lambda_)
 
     X = np.random.default_rng(1337).uniform(size=(m, input_layer_size))
     y = (np.arange(m) % num_labels).reshape(-1, 1)
@@ -49,6 +52,10 @@ if __name__ == "__main__":
     x_test = test_df.to_numpy()
     y_test = y_test.to_numpy().reshape(-1, 1)
 
+    # Normalize the data
+    x_train = x_train / 255
+    x_test = x_test / 255
+
     # Visualize 10 random images each from the training and test sets
     rng = np.random.default_rng(1337)
     (m_train, n) = x_train.shape
@@ -58,7 +65,7 @@ if __name__ == "__main__":
     fig, axs = plt.subplots(2, 10)
     for i, ax in enumerate(axs.flat[:10]):
         img = imgs[i].reshape(28, 28)
-        ax.imshow(img, cmap='gray', vmin=0, vmax=255)
+        ax.imshow(img, cmap='gray')
         ax.set_title(f'Label = {labels[i, 0]}', fontsize=6)
         ax.set_axis_off()
 
@@ -68,7 +75,7 @@ if __name__ == "__main__":
     labels = y_test[rand_idx, :]
     for i, ax in enumerate(axs.flat[10:]):
         img = imgs[i].reshape(28, 28)
-        ax.imshow(img, cmap='gray', vmin=0, vmax=255)
+        ax.imshow(img, cmap='gray')
         ax.set_title(f'Label = {labels[i, 0]}', fontsize=6)
         ax.set_axis_off()
     plt.show()
@@ -83,25 +90,21 @@ if __name__ == "__main__":
     lambda_ = 0.1
 
     # Set the models's architecture (the size of each layer)
-    architecture = [input_layer_size, hidden_layer_size,
-                    hidden_layer_size, num_labels]
-    output_layer = 'softmax'
-    mlp = MultiLayerPerceptron(architecture, lambda_=lambda_,
-                               output_layer=output_layer)
+    architecture = [
+        [input_layer_size, None],
+        [hidden_layer_size, 'relu'],
+        [hidden_layer_size, 'relu'],
+        [num_labels, 'softmax']
+    ]
+    mlp = MultiLayerPerceptron(architecture, lambda_=lambda_)
 
     # Train the model using gradient descent
     J_train_history, J_test_history = mlp.fit(x_train, y_train, x_test, y_test,
-                                              0.2, 500, batch_size=1000)
+                                              0.1, 50, batch_size=512)
 
     # Calculate model predictions on the training and test sets
     predictions_train = mlp.predict(x_train)
     predictions_test = mlp.predict(x_test)
-
-    # Calculate the mean squared error on the training and test sets
-    mse_train = mlp.mse(predictions_train, y_train)
-    mse_test = mlp.mse(predictions_test, y_test)
-    print(f'\n> Training set loss (mean squared error) : {mse_train:.4f}')
-    print(f'> Test set loss (mean squared error) : {mse_test:.4f}')
 
     # Calculate training set accuracy
     acc = np.mean(predictions_train == y_train)
@@ -128,7 +131,7 @@ if __name__ == "__main__":
     fig, axs = plt.subplots(2, 10)
     for i, ax in enumerate(axs.flat[:10]):
         img = imgs[i].reshape(28, 28)
-        ax.imshow(img, cmap='gray', vmin=0, vmax=255)
+        ax.imshow(img, cmap='gray')
         ax.set_title(f'L = {labels[i, 0]} P = {predictions[i, 0]}', fontsize=6)
         ax.set_axis_off()
 
@@ -138,7 +141,7 @@ if __name__ == "__main__":
     predictions = predictions_test[rand_idx, :]
     for i, ax in enumerate(axs.flat[10:]):
         img = imgs[i].reshape(28, 28)
-        ax.imshow(img, cmap='gray', vmin=0, vmax=255)
+        ax.imshow(img, cmap='gray')
         ax.set_title(f'L = {labels[i, 0]} P = {predictions[i, 0]}', fontsize=6)
         ax.set_axis_off()
     fig.suptitle('(L)abels vs (P)redictions')
